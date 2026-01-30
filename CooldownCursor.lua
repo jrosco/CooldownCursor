6d2735474a8f5bf72f91837444bb825165d0e30f
jrosco <2245816+jrosco@users.noreply.github.com>
2026-01-31 01:31:26 +1100

    fix: Use correct spell cooldown event to update CDs correctly
    Minor bug fixes

--- CooldownCursor.lua
+++ CooldownCursor.lua
@@ -14,6 +14,7 @@ local activeSpellID = nil
 local inCombat = false
 local fontsTable = {}
 local previewMouseMode = false
+local cachedCDSpells = {}
 
 local SHOW_WHEN_STATE = {
   ALWAYS = 0,
@@ -981,7 +982,7 @@ function CooldownCursor:Preview()
 
 
   previewActive = true
-  ShowSpellIcon(previewSpellID, false, durationObject)
+  ShowSpellIcon(previewSpellID, durationObject)
   CooldownCursor:ApplyPreviewPosition()
 
   previewTicker = C_Timer.NewTicker(durationObject:GetRemainingDuration(0), function()
@@ -1035,7 +1036,11 @@ CooldownCursor:SetScript("OnEvent", function(self, event, ...)
     return
   end
 
-  if event == "UNIT_SPELLCAST_FAILED" or event == "UNIT_SPELLCAST_SUCCEEDED" then
+  if event == "UNIT_SPELLCAST_FAILED"
+    or event == "UNIT_SPELLCAST_SUCCEEDED"
+    or event == "SPELL_UPDATE_COOLDOWN" then
+    local spellID
+    local unit
     if CooldownCursorDB.showWhen == SHOW_WHEN_STATE.NON_COMBAT and inCombat then
       return
     end
@@ -1048,8 +1053,19 @@ CooldownCursor:SetScript("OnEvent", function(self, event, ...)
       return
     end
 
-    local unit, _, spellID = ...
-    if unit ~= "player" or not spellID then return end
+    -- SPELL_UPDATE_COOLDOWN will update cooldown
+    -- that buffs, talents or items may cause.
+    if event == "SPELL_UPDATE_COOLDOWN" then
+      spellID, _, _, _ = ...
+    else
+      unit, _, spellID = ...
+      -- Cache spellID that are not SPELL_UPDATE_COOLDOWN events
+      -- This allows only tracking spells that have triggered the icon display
+      table.insert(cachedCDSpells, spellID, true)
+      if unit ~= "player" then return end
+    end
+
+    if not spellID or not cachedCDSpells[spellID] then return end
 
     -- Delay slightly to allow cooldown to register
     C_Timer.After(0.05, function()
@@ -1086,6 +1102,7 @@ end)
 -- Register events
 ----------------------------------------------------
 CooldownCursor:RegisterEvent("ADDON_LOADED")
+CooldownCursor:RegisterEvent("SPELL_UPDATE_COOLDOWN")
 CooldownCursor:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 CooldownCursor:RegisterEvent("UNIT_SPELLCAST_FAILED")
 CooldownCursor:RegisterEvent("PLAYER_REGEN_DISABLED")
